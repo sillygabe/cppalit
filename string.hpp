1e9e85c949f4a4869c1658b8d0418b41c9212d1c
sillygabe <gabrielonline630@gmail.com>
2023-11-15 12:48:40 +0300

    Fixed string :3

---
+++ string.hpp
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <string.h>
+#include "error.hpp"
+
+class String
+{
+private:
+    char * content;
+public:
+
+    char * GetChars()
+    {
+        return this->content;
+    }
+
+    int GetSize()
+    {
+        return strlen(this->content);
+    }
+
+    String()
+    {
+        this->content = (char *) malloc(0);
+    }
+
+    String(char input)
+    {
+        this->content = (char *) malloc(2);
+        this->content[0] = input; this->content[1] = '\0';
+    }
+
+    String(const char * input)
+    {
+        this->content = (char *) malloc(strlen(input) + 1);
+        this->content = strcpy(this->content, (char *) input);
+    }
+
+    String operator+(String other)
+    {
+        return strcat(this->content, other);
+    }
+
+    void operator+=(String other)
+    {
+        this->content = strcat(this->content, other);
+    }
+
+    char &operator[](const int pos)
+    {   
+        if (pos > this->GetSize() - 1) ThrowError("Array index too high");
+        if (pos < 0) ThrowError("Array index too low");
+        
+        return this->content[pos];
+    }   
+
+    operator char*()
+    {
+        return this->content;
+    }
+
+    void Reverse()
+    {
+        int a = 0, b = this->GetSize() - 1; char buff;
+        while (a < this->GetSize() / 2)
+        {
+            buff = this->operator[](a);
+            this->content[a] = this->content[b];
+            this->content[b] = buff;
+            a++; b--;
+        }
+    }
+
+    /*int Count(String ToCount)
+    {
+
+    }*/
+};
